5158c3c290b39a6f9a2fbfb79029095a0cff7154
GeorgeL <redon13@gmail.com>
2023-02-16 12:26:29 +0000

    off

---
+++ webpack.js
@@ -0,0 +1,20 @@
+
+webpack.config.js
+const path = require('path');
+
+module.exports ={
+
+    mode: 'development',
+    // The entry point file described above
+    entry: './assets/js/auth.js',
+    // The location of the build folder described above
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    watch: true,
+
+    // Optional and for development only. This provides the ability to
+    // map the built code back to the original source format when debugging.
+    // devtool: 'eval-source-map',
+};
